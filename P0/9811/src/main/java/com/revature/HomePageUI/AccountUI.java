61cad30b1c40e6822ceeec713f4f9a3259ae2395
aaronRevature <102834139+aaronRevature@users.noreply.github.com>
2022-04-14 12:38:31 -0700

    Add files via upload

--- P0/9811/src/main/java/com/revature/HomePageUI/AccountUI.java
+++ P0/9811/src/main/java/com/revature/HomePageUI/AccountUI.java
@@ -4,13 +4,15 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.revature.UIwelcome.WelcomeUI;
+import com.revature.model.user.Account;
 import com.revature.model.user.Username;
 
 public class AccountUI {
 public static void AccountUI() {
 	
+    
 	Username username = new Username();
-	
+	Account account = new Account();
 	System.out.println("Hello (usernamehere) Here are your Homepage options, Select the number you wish to use" + "\n"+
 	"1. Account balance and profile" + "\n"+
 	"2. Load/Withdraw balance" + "\n" +
@@ -22,33 +24,74 @@ public static void AccountUI() {
 	boolean MenuHome = true;
 	while(MenuHome) {
 		int userSelection =WelcomeUI.handleUserSelection(scanner);
+		scanner.nextInt();
 		switch(userSelection) {
 		  case 1:
-		    // code block
+		   
+		    	double eAcc = account.getAccountBalance();
+		    	System.out.println(""+ eAcc);
+		    try {
+		    	scanner.nextInt();
+		    }
+		    catch(InputMismatchException e) {
+				System.out.println("Sorry,invalid option.");
+		    }
 		    break;
 		  case 2:
-		    // code block
-		    break;
+			  System.out.println("To load your account type Load followed by the amount" + "\n"+
+			  "to Withdraw followed by the amount. To withdraw all your funds you can also type 'Withdraw Max'");
+			  //CODE FOR WITHDRAW AND LOAD
+			   try {
+			    	scanner.nextInt();
+			    }
+			    catch(InputMismatchException e) {
+					System.out.println("Sorry,invalid option.");
+			    }
+			   break;
 		  case 3:
-		  // code block
+			  int etran = account.getTransactions();
+			  System.out.println(""+ etran);
+			  try {
+			    	scanner.nextInt();
+			    }
+			    catch(InputMismatchException e) {
+					System.out.println("Sorry,invalid option.");
+			    }
 			break;
 		  case 4:
-	      // code block
+			  try {
+			    	scanner.nextInt();
+			    }
+			    catch(InputMismatchException e) {
+					System.out.println("Sorry,invalid option.");
+			    }
 		    break;
 		  case 5:
-	      // code block
+			  try {
+			    	scanner.nextInt();
+			    }
+			    catch(InputMismatchException e) {
+					System.out.println("Sorry,invalid option.");
+			    }
 		    break;
 		  case 6:
-		  // code block
+			  try {
+			    	scanner.nextInt();
+			    }
+			    catch(InputMismatchException e) {
+					System.out.println("Sorry,invalid option.");}
+			  
 		    break;
 		    
 		  default:
 		    // code block
 		}
+		
 
 }
- 
+
+	}
 
 }
-}
+
 
--- P0/9811/src/main/java/com/revature/model/user/Account.java
+++ P0/9811/src/main/java/com/revature/model/user/Account.java
@@ -1,16 +1,61 @@
 package com.revature.model.user;
 
-public class Account {
 
- double AccountBalance;
- int transactions;
- 
- String AuthorizedUsers;
- int AuthorizedUserAmount;
- String GamesPlayed;
- int NumGamesPlayed;
- int RewardsPoints;
- 
- 
- 
+public class Account {
+	double AccountBalance;
+	int Transactions; 
+	String[] AuthorizedUsers; 
+	int AuthorizedUserAmount;
+	String[] GamesPlayed;
+	int NumGamesPlayed;
+	 int RewardsPoints;
+	 public static int AccountCount;
+	public Account() {
+		super();
+		AccountCount++;
+	}
+	
+
+	
+	public Account(double AccountBalance, int Transactions, String[] AuthorizedUsers, int AuthorizedUserAmount, String[] GamesPlayed, int NumGamesPlayed,
+ int RewardsPoints) {
+		
+		AccountBalance = this.AccountBalance;
+		Transactions = this.Transactions; 
+		AuthorizedUsers = this.AuthorizedUsers; 
+	    AuthorizedUserAmount = this.AuthorizedUserAmount;
+		GamesPlayed = this.GamesPlayed;
+		NumGamesPlayed = this.NumGamesPlayed;
+		RewardsPoints = this.RewardsPoints;
+	}
+
+ public double getAccountBalance() {
+	return AccountBalance;
+} public int getAuthorizedUserAmount() {
+	return AuthorizedUserAmount;
+}public String[] getGamesPlayed() {
+	return GamesPlayed;
+}public int getRewards() {
+	return RewardsPoints;
+}public int getTransactions() {
+	return Transactions;
+}public int NumGamesPlayed() {
+	return NumGamesPlayed;
+}public String[] AuthorizedUsers() {
+	return AuthorizedUsers;
+}
+
+public void setAccountBalance(double AccountBalance) {
+	    AccountBalance = this.AccountBalance;
+}public void setAuthorizedUserAmount(int AuthorizedUserAmount) {
+		AuthorizedUserAmount = this.AuthorizedUserAmount;
+}public void setGamesPlayed(String[] GamesPlayed) {
+	    GamesPlayed = this.GamesPlayed;
+}public void setRewardsPoints(int RewardsPoints) {
+	   RewardsPoints = this.RewardsPoints;
+}public void setTransactions(int Transactions) {
+	   Transactions = this.Transactions;
+}public void SetNumGamesPlayed(int NumGamesPlayed) {
+	   NumGamesPlayed = this.NumGamesPlayed;
+}
 }
